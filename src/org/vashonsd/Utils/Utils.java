61951bd65d618381d16b6acf588e09b5e5953d63
BeckersAmaze <reid.beckett@vashonsd.org>
2018-05-18 08:42:06 -0700

    Works, but I still need to make the method that checks whether each guessed letter is in the word you are guessing.

--- src/org/vashonsd/Utils/Utils.java
+++ src/org/vashonsd/Utils/Utils.java
@@ -1,6 +1,8 @@
 package org.vashonsd.Utils;
 
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 
 public class Utils {
@@ -26,4 +28,17 @@ public class Utils {
         Random rand = new Random();
         return rand.nextInt(5)+1;
     }
+
+//    public static ArrayList isInGoalWord(char letter, String goalWord, String correctGuesses){
+//
+//        for(int i = 0; i < goalWord.length(); i++){
+//            if(goalWord.substring(i).equalsIgnoreCase("" + letter)){
+//                correctGuesses.replace(correctGuesses.substring(i, i + 1), letter);
+//
+//            }
+//
+//        }
+//
+//
+//    }
 }
