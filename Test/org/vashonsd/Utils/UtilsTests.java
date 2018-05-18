00f054f1281a9fa0e75d2f97b3f5a367d98f987a
noahedmonds <edmonds.noah@vashonsd.org>
2018-05-18 08:43:18 -0700

    It works

--- Test/org/vashonsd/Utils/UtilsTests.java
+++ Test/org/vashonsd/Utils/UtilsTests.java
@@ -2,6 +2,11 @@ package org.vashonsd.Utils;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.vashonsd.Games.NE.Choice;
+import org.vashonsd.Games.NE.Paper;
+import org.vashonsd.Games.NE.Rock;
+import org.vashonsd.Games.NE.Scissors;
+
 
 /**
  * Created by andy on 5/2/18.
@@ -9,7 +14,16 @@ import org.junit.Test;
 public class UtilsTests {
     @Test
     public void testIsInteger(){
-        Assert.assertTrue(Utils.IsInteger("3"));
+       Assert.assertTrue(Utils.IsInteger("3"));
         Assert.assertFalse(Utils.IsInteger("th4n"));
+    }
+
+    @Test
+    public void testCardComparison() {
+        Choice c = new Rock();
+        Assert.assertEquals(c.determineWinner(new Paper()), "lose");
+        Assert.assertEquals(c.determineWinner(new Scissors()), "win");
+        Assert.assertEquals(c.determineWinner(new Rock()), "tie");
+
     }
 }
